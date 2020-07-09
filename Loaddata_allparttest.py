4bd2925c33a3873f46d759bade2cecca337f538c
Mavis <53061877+lungproject@users.noreply.github.com>
2020-07-08 22:15:38 -0400

    Update Loaddata_allparttest.py

--- Loaddata_allparttest.py
+++ Loaddata_allparttest.py
@@ -1,12 +1,25 @@
 import os  
-from PIL import Image  
+
 import csv
 import numpy as np  
 from keras import backend as K
 import scipy.io
  
 
+def load_sampledata():
+    
+    img = np.load("./Sampledata/xpsamplepet.npy") #Deeplearningallpatchsmalls
+    datapet = np.asarray(img,dtype="float32")
+    img = np.load("./Sampledata/xpsamplect.npy") #Deeplearningallpatchsmalls
+    datact = np.asarray(img,dtype="float32")
+    img = np.load("./Sampledata/xpsamplefuse.npy") #Deeplearningallpatchsmalls
+    datafuse = np.asarray(img,dtype="float32")
+    y = np.load("./Sampledata/label.npy")
+    label = np.asarray(y,dtype="float32")
+    
 
+    return datapet,datact,datafuse,label
+   
 def load_SPHtraindata():
     
     img = np.load("./Alldata/sphxptrainpet2_64.npy") #Deeplearningallpatchsmalls
